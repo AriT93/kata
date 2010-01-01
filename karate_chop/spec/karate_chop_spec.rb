d28082f7454d89eb6cd0cf281d3c9dd6eebed24f
Ari <abturet@edipa.(none)>
2009-12-31 20:07:53 -0600

    single compare

--- karate_chop/spec/karate_chop_spec.rb
+++ karate_chop/spec/karate_chop_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 def t_chop(val, array)
-  return Kc.chop_iteritive(val, array)
+  return Kc.chop_single(val, array)
 end
 describe Karate do
   before(:all) do
