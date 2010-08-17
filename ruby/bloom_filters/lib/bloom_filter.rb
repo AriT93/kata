f7ec7bd27afa356e60c28d7146530ea36727d64c
AriT93 <arit93@yahoo.com>
2010-08-17 14:34:42 -0500

    added bloom filter kata and some housekeeping

---
+++ ruby/bloom_filters/lib/bloom_filter.rb
@@ -0,0 +1,34 @@
+require 'digest/md5'
+class BloomFilter
+
+  def initialize(size, hashses, word_list)
+    @bitmap = Array.new(size)
+    @bitmap.fill(0)
+    @hashes = Array.new(hashses){ |i| BloomHash.new(i,size - 1)}
+
+    File.new(word_list, 'r').each_line do |word|
+      @hashes.each{|h| @bitmap[h.compute(word.downcase.strip)] = 1}
+    end
+  end
+  def search(word)
+    @hashes.each { |h| return false if @bitmap[h.compute(word.downcase.strip)] == 0}
+    return true
+  end
+end
+
+class BloomHash
+  def initialize(seed, max_value)
+    @offset = seed
+    @num_digits = 0
+    @max_value = max_value
+    while(max_value > 0) do
+      @num_digits += 1
+      max_value >>= 4
+    end
+  end
+
+  def compute(word)
+    digest = Digest::MD5.hexdigest(word)
+    return digest[@offset, @num_digits].to_i(16) % @max_value
+  end
+end
---
+++ ruby/bloom_filters/spec/bloom_filter_spec.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+
+describe BloomFilter do
+  before (:all) do
+    BF = BloomFilter.new(1000000,4,"data/wordlist.txt")
+  end
+
+  it "should fail search for dribble" do
+    BF.search("dribble").should == false
+  end
+
+  it "should succedd search for driving" do
+    BF.search("driving").should == true
+  end
+end
