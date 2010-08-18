3c15397c6c37644708ef6fffedd0a7f4a7431a0c
AriT93 <arit93@yahoo.com>
2010-08-18 13:27:46 -0500

    second impl of bloom filter

--- ruby/bloom_filters/lib/bloom_filter.rb
+++ ruby/bloom_filters/lib/bloom_filter.rb
@@ -1,39 +1,30 @@
 require 'digest/md5'
 #so far this is stolen from various places on the web to help learning.
 #I still need to do a clean impl on my own
-
+# ok played around and this works.  also I think I could pass in a second block here too.
+# this is inspired by Peter cooper's bloomingsimple code
 class BloomFilter
 
-  def initialize(size, hashses, word_list)
-    @bitmap = Array.new(size)
+  def initialize(size, hashses, word_list, &block)
+    @size = size
+    @bitmap = Array.new(@size)
     @bitmap.fill(0)
-    @hashes = Array.new(hashses){ |i| BloomHash.new(i,size - 1)}
+
+    @hasfunc = block || lambda do |word|
+      digest = Digest::MD5.hexdigest(word)
+      [h1 =  digest[1,5].to_i(16) % @size -1, h2 = word.hash, word.size ** 2]
+    end
 
     File.new(word_list, 'r').each_line do |word|
-      @hashes.each{|h| @bitmap[h.compute(word.downcase.strip)] = 1}
+      add(word.strip)
     end
   end
+
   def search(word)
-    @hashes.each { |h| return false if @bitmap[h.compute(word.downcase.strip)] == 0}
-    return true
-  end
-end
-
-
-
-class BloomHash
-  def initialize(seed, max_value)
-    @offset = seed
-    @num_digits = 0
-    @max_value = max_value
-    while(max_value > 0) do
-      @num_digits += 1
-      max_value >>= 4
-    end
+    @hasfunc[word.strip].each { |w| return false unless @bitmap[w % @size] == 1} and true
   end
 
-  def compute(word)
-    digest = Digest::MD5.hexdigest(word)
-    return digest[@offset, @num_digits].to_i(16) % @max_value
+  def add(word)
+    @hasfunc[word].each{ |w| @bitmap[w % @size] = 1}
   end
 end
--- ruby/bloom_filters/spec/bloom_filter_spec.rb
+++ ruby/bloom_filters/spec/bloom_filter_spec.rb
@@ -1,6 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-
 describe BloomFilter do
   before (:all) do
     BF = BloomFilter.new(1000000,4,"data/wordlist.txt")
