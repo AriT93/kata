4de72094990664290920da14c2566f14e74b6b30
Ari Turetzky <arit93@gmail.com>
2018-03-12 10:14:10 -0700

    updated to simple recursion.
    I think there is probably a better way to do it but this
    seems to be a very ruby-ish way

--- ruby/karate_chop/lib/karate_chop.rb
+++ ruby/karate_chop/lib/karate_chop.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
+# Karate Chop Class for Kata
 class KarateChop
   def chop(num, arr)
-    arr.index(num) || -1
+    chop_inner(num, arr, arr.size, 0)
+  end
+
+  def chop_inner(num, arr, high, low)
+    return -1 if low > high
+    mid = low + ((high - low) / 2)
+    return chop_inner(num, arr, mid - 1, low)  if
+      arr[mid].to_i > num
+    return chop_inner(num, arr, high, mid + 1) if
+      arr[mid].to_i < num
+    mid
   end
 end
--- ruby/karate_chop/spec/karate_chop_spec.rb
+++ ruby/karate_chop/spec/karate_chop_spec.rb
@@ -24,5 +24,4 @@ RSpec.describe KarateChop do
   Then { karate_chop.chop(4, [1, 3, 5, 7]) == -1 }
   Then { karate_chop.chop(6, [1, 3, 5, 7]) == -1 }
   Then { karate_chop.chop(8, [1, 3, 5, 7]) == -1 }
-
 end
